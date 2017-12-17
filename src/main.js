437c8aaa812a784287fd2321f47370cf50926a8b
Emily Carey <emilyrosinacarey@gmail.com>
2017-12-17 18:57:47 +0000

    updated packages and added vue-awesome for icons

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable no-unused-vars */
 import Vue from 'vue'
 import App from './App'
 import store from './store'
 import router from './router'
+import ElementUI from 'element-ui'
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/main.scss'
+
+Vue.use(ElementUI)
+Vue.component('icon', Icon)
+// require('element-ui/lib/theme-chalk/index.css')
+// require('@/assets/main.scss')
 
 Vue.config.productionTip = false
-Vue.use(require('element-ui'))
-require('element-ui/lib/theme-chalk/index.css')
-require('@/assets/main.scss')
 
 /* eslint-disable no-new */
 new Vue({
