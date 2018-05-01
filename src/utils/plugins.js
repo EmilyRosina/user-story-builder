a5e37fbb55287ce00a63c59a3d572a47b47e0e92
Emily Rosina Carey <emilyrosinacarey@gmail.com>
2018-05-01 14:25:10 +0100

    Add google analytics

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -9,5 +9,19 @@ import ElementUIStyles from 'element-ui/lib/theme-chalk/index.css'
 import Icon from 'vue-awesome/components/Icon'
 import VueAwesomeIcons from 'vue-awesome/icons'
 
+// Vue Analytics
+import vueAnalytics from 'vue-analytics'
+import router from '@/router'
+
 Vue.use(ElementUI)
 Vue.component('icon', Icon)
+Vue.use(vueAnalytics, {
+  id: 'UA-118476098-1',
+  debug: {
+    sendHitTask: process.env.NODE_ENV === 'production'
+  },
+  router,
+  autoTracking: {
+    shouldRouterUpdate (to, from) { return to.path !== from.path }
+  }
+})
